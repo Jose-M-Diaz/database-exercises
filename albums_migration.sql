701a8d89c57271f5157eb6b260d88935319e51cc
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-01-25 10:02:48 -0600

    completed mysql table exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+     id INT UNSIGNED AUTO_INCREMENT,
+     artist VARCHAR(200),
+     name VARCHAR(200),
+     release_date INT UNSIGNED,
+     sales FLOAT UNSIGNED,
+     genre VARCHAR(100),
+     PRIMARY KEY (id)
+     );
+SHOW TABLES;
