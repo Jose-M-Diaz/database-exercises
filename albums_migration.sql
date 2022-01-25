46a1d583b5a05d8d7259d77262e0f94d1530a3a7
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-01-25 10:38:32 -0600

    completed walkthrough

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,14 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-     id INT UNSIGNED AUTO_INCREMENT,
-     artist VARCHAR(200),
-     name VARCHAR(200),
+     id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+     artist VARCHAR(200) NOT NULL,
+     name VARCHAR(200) NOT NULL,
      release_date INT UNSIGNED,
      sales FLOAT UNSIGNED,
      genre VARCHAR(100),
      PRIMARY KEY (id)
      );
 SHOW TABLES;
+DESCRIBE albums;
+SHOW CREATE TABLE albums;
