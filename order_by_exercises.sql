39eadf8846f1d1b57f942be834c1c46f95466aeb
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-01-27 09:50:17 -0600

    completed order_by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,25 @@
+USE employees;
+
+# SELECT * FROM employees WHERE  first_name IN ('Irena', 'Vidya') OR first_name = 'Maya';
+SELECT *
+FROM employees
+WHERE  gender = 'M'
+  AND (first_name IN ('Irena', 'Vidya') OR first_name = 'Maya')
+ORDER BY last_name, first_name;
+
+# SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT *
+FROM employees
+WHERE last_name
+          LIKE 'E%'
+   OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE last_name
+          LIKE 'E%E'
+ORDER BY emp_no DESC;
+
+# SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
