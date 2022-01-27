7a5111304c4d48d5b0753b4e95e32f53c31c5bc4
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-01-27 09:28:22 -0600

    completed walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,5 +5,8 @@ SELECT * FROM employees WHERE  gender = 'M' AND (first_name IN ('Irena', 'Vidya'
 
 # SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%E';
 
 # SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
+
